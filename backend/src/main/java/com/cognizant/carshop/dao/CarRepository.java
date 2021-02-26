90ff9eb4230b878bd698693856f2680293242a2c
gunerhanale <gunerhanale@gmail.com>
2021-02-26 02:46:11 +0100

    added new Rest API method that is findByWarehouseId

--- backend/src/main/java/com/cognizant/carshop/dao/CarRepository.java
+++ backend/src/main/java/com/cognizant/carshop/dao/CarRepository.java
@@ -1,8 +1,14 @@
 package com.cognizant.carshop.dao;
 
 import com.cognizant.carshop.entity.Car;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
 
+@CrossOrigin("http://localhost:4200")
 public interface CarRepository extends JpaRepository<Car,Long> {
 
+    Page<Car> findByWarehouseId(@RequestParam("id") Long id, Pageable pageable);
 }
